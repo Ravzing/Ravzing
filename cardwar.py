0e1980816aa4f045a658cb4a96bc32d5e3a3f567
Ravzing <audriusez@gmail.com>
2025-01-06 22:18:59 +0200

    Created some of the rules

--- cardwar.py
+++ cardwar.py
@@ -1,12 +1,38 @@
 import random
 
-suit = ["S","H","C","D"]
-
 deck = []
 
-for suit in suit:
-    for card in range(2,15):
-        deck.append((card,suit))
-
-random.shuffle(deck)        
-print(deck)
+def create_deck():
+    for suit in ["S","H","C","D"]:
+        for card in range(2,15):
+            deck.append((card,suit))
+            
+def draw_card(player):
+    card = deck[0]
+    deck.remove(deck[0])
+    print(player + " drew the " + str(card))
+    return card
+    
+def game_rules():
+    while True:
+        player_one = draw_card("player one")
+        player_two = draw_card("player two")
+        if player_one[0] > player_two[0]:
+            winner = "player one"
+        elif player_one[0] < player_two[0]:
+            winner = "player two"
+        else: 
+            winner = "tie"
+            
+        print(winner + " win")
+        
+        if len(deck) == 0:
+            print("deck is empty")
+            break
+def main():
+    create_deck()
+    random.shuffle(deck)  
+    game_rules()
+    
+if __name__=="__main__":
+    main()
